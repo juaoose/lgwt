9d04daecc04c214a6583d9bc1d2bf04786da4888
Juaoose <jjrg1994@gmail.com>
2021-11-17 18:45:53 -0500

    add iteration example

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 6)
@@ -9,6 +12,12 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 11)
+	fmt.Println(repeated)
+	// Output: aaaaaaaaaaa
 }
 
 // To run the benchmark, use: `go test -bench=.`
