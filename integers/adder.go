5ac41176b3b841234117deb1144feba1b0b6f09a
Juaoose <jjrg1994@gmail.com>
2021-11-17 16:28:18 -0500

    add example

--- integers/adder.go
+++ integers/adder.go
@@ -1,5 +1,8 @@
 package integers
 
+// Adds two integers and returns the result
+// Note: when you have more than one argument of the same type you can shorting from
+// (a int, b int) to (a, b int)
 func Add(a, b int) int {
 	return a + b
 }
--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,7 @@
 package integers
 
 import "testing"
+import "fmt"
 
 func TestAdd(t *testing.T) {
 	sum := Add(1, 2)
@@ -9,4 +10,10 @@ func TestAdd(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(5, 6)
+	fmt.Println(sum)
+	// Output: 11
 }
