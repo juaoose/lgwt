9e18231388a9596c1f9aba780cd6437b870bc532
Juaoose <jjrg1994@gmail.com>
2021-11-17 12:00:45 -0500

    add comments

--- hello-empty/hello_empty_test.go
+++ hello-empty/hello_empty_test.go
@@ -5,6 +5,7 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// This marks the function as a helper, so that if the test fails the caller is reported instead of this function
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -14,14 +15,12 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("Juan")
 		want := "Hello, Juan"
-
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
-
 		assertCorrectMessage(t, got, want)
 	})
 
