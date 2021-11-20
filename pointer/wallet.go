b5f2133d5e8520fab99192d13600c06ce38cd193
Juaoose <jjrg1994@gmail.com>
2021-11-19 21:16:10 -0500

    add pointers and errors

--- pointer/wallet.go
+++ pointer/wallet.go
@@ -1,14 +1,36 @@
 package wallet
 
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // https://gobyexample.com/pointers
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -2,14 +2,63 @@ package wallet
 
 import "testing"
 
-func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(1)
+// You can use errcheck to find missing checks
+// go get -u github.com/kisielk/errcheck
 
+func TestWallet(t *testing.T) {
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(15)}
+		err := wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(5)
+		assertBalance(t, wallet, want)
+		assertNoError(t, err)
+
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		initialFunds := Bitcoin(15)
+		wallet := Wallet{balance: initialFunds}
+		err := wallet.Withdraw(Bitcoin(16))
+
+		assertBalance(t, wallet, initialFunds)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
+
+func assertError(t *testing.T, err error, want error) {
+	t.Helper()
+	if err == nil {
+		// Fatal stops the test
+		t.Fatal("wanted an error but got none")
+	}
+
+	if err != want {
+		t.Errorf("got %q, want %q", err, want)
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
 	got := wallet.Balance()
-	want := 1
 
 	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		t.Errorf("got %s want %s", got, want)
 	}
 }
