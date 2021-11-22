131c3c1ad3417787eb50f10a3987768861bd23a1
Juaoose <jjrg1994@gmail.com>
2021-11-22 10:31:29 -0500

    add select

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,22 +8,40 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
+	t.Run("compare speed of servers", func(t *testing.T) {
+		slowServer := makeDelayedServer(9 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	// Call at the end of the containing function
-	defer slowServer.Close()
-	defer fastServer.Close()
+		// Call at the end of the containing function
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		want := fastURL
+		got, err := Racer(slowURL, fastURL)
 
-	if got != want {
-		t.Errorf("got '%s', want '%s'", got, want)
-	}
+		if err != nil {
+			t.Fatalf("no error expected but got %v", err)
+		}
+
+		if got != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	})
+
+	t.Run("return error if no response after specified time", func(t *testing.T) {
+		server := makeDelayedServer(25 * time.Millisecond)
+
+		defer server.Close()
+
+		_, err := ConfigurableRacer(server.URL, server.URL, 20*time.Millisecond)
+
+		if err == nil {
+			t.Error("expected an error but got none")
+		}
+	})
 
 }
 
