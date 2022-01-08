d810c0db375cc0825cba5b187aec407c4c109991
Juaoose <jjrg1994@gmail.com>
2022-01-07 22:28:47 -0500

    update mod

--- math/clock_test.go
+++ math/clock_test.go
@@ -1,16 +1,18 @@
-package clock
+package clock_test
 
 import (
 	"testing"
 	"time"
+
+	"github.com/juaoose/lgwt/math/clock"
 )
 
 func TestSecondHandAtMidnight(t *testing.T) {
 	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
 	// Draw a line up with length 90
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
+	want := clock.Point{X: 150, Y: 150 - 90}
+	got := clock.SecondHand(tm)
 
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
