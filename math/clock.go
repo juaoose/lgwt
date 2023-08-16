1a4de84ed6d28a4ff36364430437844685b48236
Juan <juaoose@users.noreply.github.com>
2023-08-16 18:54:16 -0500

    simplify math files

--- math/clock.go
+++ math/clock.go
@@ -10,30 +10,36 @@ type Point struct {
 	Y float64
 }
 
-const (
-	clockCenter      = 150
-	secondHandLength = 90
-)
-
-func SecondHand(t time.Time) Point {
-	p := secondHandPoint(t)
-
-	// hand length is 90
-	// since svg origin is top left
-	// our origin is 150,150
-	p = Point{p.X*secondHandLength + clockCenter, -p.Y*secondHandLength + clockCenter}
-	return p
-}
-
 func secondsInRadians(t time.Time) float64 {
 	// 1s = 2pi/60 radians
 	return (math.Pi / (30 / (float64(t.Second()))))
 }
 
-func secondHandPoint(t time.Time) Point {
-	angle := secondsInRadians(t)
+func minutesInRadians(t time.Time) float64 {
+	return (secondsInRadians(t) / 60) +
+		(math.Pi / (30 / float64(t.Minute())))
+}
+
+func hoursInRadians(t time.Time) float64 {
+	return (minutesInRadians(t) / 12) +
+		(math.Pi / (6 / float64(t.Hour()%12)))
+}
+
+func angleToPoint(angle float64) Point {
 	x := math.Sin(angle)
 	y := math.Cos(angle)
 
 	return Point{x, y}
+}
+
+func secondHandPoint(t time.Time) Point {
+	return angleToPoint(secondsInRadians(t))
+}
+
+func minuteHandPoint(t time.Time) Point {
+	return angleToPoint(minutesInRadians(t))
+}
+
+func hourHandPoint(t time.Time) Point {
+	return angleToPoint(hoursInRadians(t))
 }
--- math/clock_test.go
+++ math/clock_test.go
@@ -22,7 +22,7 @@ func TestSecondsInRadians(t *testing.T) {
 		t.Run(testName(c.time), func(t *testing.T) {
 			got := secondsInRadians(c.time)
 
-			if got != c.angle {
+			if !aproxFloat64Equal(got, c.angle) {
 				t.Errorf("got %v radians, want %v radians", got, c.angle)
 			}
 		})
@@ -36,6 +36,46 @@ func TestSecondsInRadians(t *testing.T) {
 
 	if got != want {
 		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func TestMinutesInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(0, 30, 0), math.Pi},
+		{simpleTime(0, 0, 7), 7 * (math.Pi / (30 * 60))},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := minutesInRadians(c.time)
+			if !aproxFloat64Equal(got, c.angle) {
+				t.Errorf("got %v, want %v", got, c.angle)
+			}
+		})
+	}
+}
+
+func TestHoursInRadians(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		angle float64
+	}{
+		{simpleTime(6, 0, 0), math.Pi},
+		{simpleTime(0, 0, 0), 0},
+		{simpleTime(21, 0, 0), math.Pi * 1.5},
+		{simpleTime(0, 1, 30), math.Pi / ((6 * 60 * 60) / 90)},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(t *testing.T) {
+			got := hoursInRadians(c.time)
+			if !aproxFloat64Equal(got, c.angle) {
+				t.Errorf("got %v, want %v", got, c.angle)
+			}
+		})
 	}
 }
 
@@ -53,6 +93,45 @@ func TestSecondHandPoint(t *testing.T) {
 			got := secondHandPoint(c.time)
 
 			// Since this is not critical, we have an approximate equality check
+			if !aproxPointEqual(got, c.point) {
+				t.Errorf("got %v, want %v", got, c.point)
+			}
+		})
+	}
+}
+
+func TestMinuteHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(0, 30, 0), Point{0, -1}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(*testing.T) {
+			got := minuteHandPoint(c.time)
+
+			if !aproxPointEqual(got, c.point) {
+				t.Errorf("got %v, want %v", got, c.point)
+			}
+		})
+	}
+}
+
+func TestHourHandPoint(t *testing.T) {
+	cases := []struct {
+		time  time.Time
+		point Point
+	}{
+		{simpleTime(6, 0, 0), Point{0, -1}},
+		{simpleTime(21, 0, 0), Point{-1, 0}},
+	}
+
+	for _, c := range cases {
+		t.Run(testName(c.time), func(*testing.T) {
+			got := hourHandPoint(c.time)
+
 			if !aproxPointEqual(got, c.point) {
 				t.Errorf("got %v, want %v", got, c.point)
 			}
